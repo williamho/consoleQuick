8692d2ae20cf60650bf664e06bba11bf6263db78
William Ho <wwwilliamho@gmail.com>
2016-01-11 11:23:37 -0500

    Add example of specs2 mockito failing test

--- build.sbt
+++ build.sbt
@@ -10,37 +10,7 @@ scalacOptions ++= Seq("-feature","-deprecation", "-unchecked", "-Xlint")
 scalacOptions in Test ++= Seq("-Yrangepos")
 
 libraryDependencies ++= Seq(
-  "com.chuusai" %% "shapeless" % "2.2.5",
-  "com.typesafe" % "config" % "1.3.0",
-  "com.typesafe.play" %% "play-json" % "2.4.3",
-  "org.specs2" %% "specs2-core" % "3.0" % "test",
-  "org.specs2" %% "specs2-mock" % "3.0" % "test"
+  "org.specs2" %% "specs2-core" % "3.7" % "test",
+  "org.specs2" %% "specs2-mock" % "3.7" % "test"
 )
-
-// Commands to run on `sbt console` or `sbt consoleQuick`
-initialCommands := """
-  import com.typesafe.config._
-  import play.api.libs.json._
-  import scala.concurrent.ExecutionContext.Implicits.global
-  import scala.concurrent._
-  import scala.concurrent.duration._
-
-  import shapeless._
-  import shapeless.ops.function._
-  import shapeless.ops.hlist._
-  import shapeless.ops.record._
-  import shapeless.record._
-  import shapeless.syntax.singleton._
-  import shapeless.syntax.std._
-  import shapeless.syntax.std.function._
-  import shapeless.syntax.std.traversable._
-
-  implicit val timeout = 10.seconds
-
-  // Utility for evaluating futures
-  // e.g., Future.successful("Hello").await
-  implicit class FutureWithAwait[T](f: Future[T]) {
-    def await() = Await.result(f, 10.seconds)
-  }
-"""
 
--- src/test/scala/Spec.scala
+++ src/test/scala/Spec.scala
@@ -1,25 +1,46 @@
 import org.specs2.mutable._
 import org.specs2.mock.Mockito
 
-import com.typesafe.config._
-import play.api.libs.json._
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent._
-import scala.concurrent.duration._
-
-import shapeless._
-import shapeless.ops.function._
-import shapeless.ops.hlist._
-import shapeless.ops.record._
-import shapeless.record._
-import shapeless.syntax.singleton._
-import shapeless.syntax.std._
-import shapeless.syntax.std.function._
-import shapeless.syntax.std.traversable._
-
 class Spec extends Specification with Mockito {
-  "Hello world" >> {
-    1 must_== 1
+  class Timer {
+    // Want to mock this method to just return `body`
+    def time(body: => String): String = {
+      val start = System.currentTimeMillis()
+      val result = body
+      val end = System.currentTimeMillis()
+      println(s"${end - start}ms")
+
+      result
+    }
+
+    def thisShouldBeCalledOnce(): String = "hello"
+  }
+
+  class MyClass(timer: Timer) {
+    def doSomething(): String = {
+      //println("inside method `doSomething`")
+
+      timer.time {
+        //println("inside method `doSomething` in `timer.time`")
+        timer.thisShouldBeCalledOnce()
+      }
+    }
+  }
+
+  "timer.thisShouldBeCalledOnce should be called once" >> {
+    val timer = mock[Timer]
+    timer.thisShouldBeCalledOnce returns "hi"
+
+    timer.time(anyString) answers { (params: Any, mock: Any) =>
+      //println("inside `answers`, before params match")
+      params match {
+        case Array(f: Function0[String] @unchecked) => f()
+      }
+    }
+
+    val c = new MyClass(timer)
+    c.doSomething()
+    there was one(timer).thisShouldBeCalledOnce() // Is actually called 3 times
   }
 }
 
